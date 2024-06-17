32cb8985213e7a1f7b0445bdd87f22cb5e118d3c
UnkaiDev <vanbv94@gmail.com>
2024-06-17 15:49:33 +0700

    Part 6 : Getting all the initialzation parameters

---
+++ src/main/java/DemoServlet.java
@@ -0,0 +1,36 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		ServletConfig config = getServletConfig();
+		Enumeration<String> e = config.getInitParameterNames();
+		String str = "";
+		while (e.hasMoreElements()) {
+			str = e.nextElement();
+			out.print("<br>Name: " + str);
+			out.print(" value: " + config.getInitParameter(str));
+		}
+		out.close();
+	}
+}
