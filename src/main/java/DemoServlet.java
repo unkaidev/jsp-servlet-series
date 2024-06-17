a8fc16f4e242bcbf0df29df80e0df25ff1294ebd
UnkaiDev <vanbv94@gmail.com>
2024-06-17 15:38:18 +0700

    Part 5 : Getting one intialization parameter from the web.xml

---
+++ src/main/java/DemoServlet.java
@@ -0,0 +1,33 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		ServletConfig config = getServletConfig();
+		String driver = config.getInitParameter("driver");
+		pw.print("Driver is: " + driver);
+		pw.close();
+	}
+
+}
