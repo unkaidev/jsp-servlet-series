1063c669683abdefd75efa1ee7934de866346feb
UnkaiDev <vanbv94@gmail.com>
2024-06-17 16:08:44 +0700

    Part 7 : Get the initialization parameter

---
+++ src/main/java/DemoServlet.java
@@ -0,0 +1,35 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+
+		ServletContext context = getServletContext();
+
+		String driverName = context.getInitParameter("dname");
+		pw.print("Driver name is = " + driverName);
+		pw.close();
+
+	}
+
+}
