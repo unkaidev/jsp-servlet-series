589b9dccbcdb23e69abe8ee55e8b12ba66efa922
UnkaiDev <vanbv94@gmail.com>
2024-06-17 15:22:26 +0700

    Part 4: Use sendRedirect() method

---
+++ src/main/java/MySeacher.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MySeacher
+ */
+public class MySeacher extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name = request.getParameter("name");
+		response.sendRedirect("https://www.google.com.vn/search?q=" + name);
+	}
+}
