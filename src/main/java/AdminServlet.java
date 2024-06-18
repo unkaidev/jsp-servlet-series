e9a08ac93eda87f3e79d3b1a82c6c0655781bde6
UnkaiDev <vanbv94@gmail.com>
2024-06-18 09:18:58 +0700

    Part 17 : Authentication user with filter

---
+++ src/main/java/AdminServlet.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.print("Welcome ADMIN");
+		pw.close();
+	}
+
+}
---
+++ src/main/java/MyFilter.java
@@ -0,0 +1,59 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+public class MyFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public MyFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		PrintWriter pw = response.getWriter();
+		String password = request.getParameter("password");
+		if (password.equals("admin")) {
+			chain.doFilter(request, response);
+		} else {
+			pw.print("Username or Password was wrong!");
+			RequestDispatcher rq = request.getRequestDispatcher("/index.html");
+			rq.include(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
