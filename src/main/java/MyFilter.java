621f833bc6873abdd72b1326116e45560a555a10
UnkaiDev <vanbv94@gmail.com>
2024-06-18 08:50:02 +0700

    Part 16 : Use Filter

---
+++ src/main/java/MyFilter.java
@@ -0,0 +1,56 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+public class MyFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public MyFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+
+		PrintWriter out = response.getWriter();
+		out.print("filter is invoked before");
+
+		chain.doFilter(request, response);// sends request to next resource
+
+		out.print("filter is invoked after");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
